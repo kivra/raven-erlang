bc4ef1dbd7b0006e9654afb019bdae2810b5d35b
Hakan Nilsson <hakan.nilsson@kivra.com>
2017-10-03 14:51:06 +0200

    crypto:rand_uniform is deprecated, use rand:uniform instead

--- src/raven.erl
+++ src/raven.erl
@@ -124,11 +124,11 @@ get_config(App) ->
 
 
 event_id_i() ->
-	U0 = crypto:rand_uniform(0, (2 bsl 32) - 1),
-	U1 = crypto:rand_uniform(0, (2 bsl 16) - 1),
-	U2 = crypto:rand_uniform(0, (2 bsl 12) - 1),
-	U3 = crypto:rand_uniform(0, (2 bsl 32) - 1),
-	U4 = crypto:rand_uniform(0, (2 bsl 30) - 1),
+	U0 = rand:uniform((2 bsl 32) - 1) - 1,
+	U1 = rand:uniform((2 bsl 16) - 1) - 1,
+	U2 = rand:uniform((2 bsl 12) - 1) - 1,
+	U3 = rand:uniform((2 bsl 32) - 1) - 1,
+	U4 = rand:uniform((2 bsl 30) - 1) - 1,
 	<<UUID:128>> = <<U0:32, U1:16, 4:4, U2:12, 2#10:2, U3:32, U4:30>>,
 	iolist_to_binary(io_lib:format("~32.16.0b", [UUID])).
 
