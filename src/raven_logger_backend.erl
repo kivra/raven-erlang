3447f637d41447cf4483d0e324c8344ed75b5564
Robert Svensen <robert.svensen@kivra.com>
2021-10-15 16:32:16 +0200

    Handle more types of log messages

--- src/raven_logger_backend.erl
+++ src/raven_logger_backend.erl
@@ -7,7 +7,8 @@
 log(LogEvent, _Config) ->
 	case is_raven_log(LogEvent) of
 		true  -> ok; % Dropping raven log, prevents log loop
-		false -> raven:capture(LogEvent, [])
+		false ->
+		raven:capture(get_msg(LogEvent), parse_message(LogEvent))
 	end.
 
 is_raven_log(#{meta := Meta} = _LogEvent) ->
@@ -16,4 +17,30 @@ is_raven_log(#{meta := Meta} = _LogEvent) ->
 		true  -> #{report_cb := Report} = Meta,
 				 Report =:= fun ssl_logger:format/1
 	end.
+
+get_msg(#{msg := MsgList} = _LogEvent) ->
+	case MsgList of
+		{string, Msg}  -> Msg;
+		{report, _Msg} -> "rapport";
+		{_, _Msg}      -> "not expected"
+	end.
+
+parse_message(LogEvent) ->
+	Meta = maps:get(meta, LogEvent),
+	Msg  = get_msg(LogEvent),
+	Level = maps:get(level, LogEvent),
+	[
+		{level, Level},
+%		{exception, {Class, Reason}},
+%		{stacktrace, Stacktrace},
+		{extra, [
+			{name, "Name"},
+			{pid, maps:get(pid, Meta)},
+			{last_event, "LastEvent"},
+			{state_name, "StateName"},
+			{state_data, "StateData"},
+			{callback_mode, "CallbackMode"},
+			{reason, LogEvent}
+		]}
+	].
 
