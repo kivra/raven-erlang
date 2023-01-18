6fd226fd431c1003e0737293c3e901985049cc32
Christian Sunesson <christian.sunesson@kivra.com>
2023-01-18 15:06:07 +0100

    remove error_logger backend

--- src/raven_app.erl
+++ src/raven_app.erl
@@ -1,10 +1,10 @@
 -module(raven_app).
+-behavior(application).
 -export([
 	start/0,
 	stop/0
 ]).
 
--behaviour(application).
 -export([
 	start/2,
 	stop/1
@@ -31,44 +31,27 @@ start(_StartType, _StartArgs) ->
             persistent_term:put(?RAVEN_SSL_PERSIST_KEY, {ssl, []})
     end,
     {ok, _ProfilePid} = inets:start(httpc, [{profile, ?RAVEN_HTTPC_PROFILE}]),
-	case application:get_env(uri) of
-		{ok, _} ->
-			case application:get_env(error_logger) of
-				{ok, true} ->
-					error_logger:add_report_handler(raven_error_logger);
-				_ -> ok
-			end,
-			case application:get_env(otp_logger) of
-				{ok, true} ->
-					logger:add_handler(raven_otp_logger, raven_logger_backend, #{level => warning
-						, filter_default => log
-						, filters => [{ssl,      {fun logger_filters:domain/2, {stop, sub, [ssl]}}}
-									 ,{progress, {fun logger_filters:domain/2, {stop, equal, [progress]}}}
-									 ,{raven,    {fun logger_filters:domain/2, {stop, sub, [raven]}}}
-						             ,{sasl,     {fun logger_filters:domain/2, {stop, sub, [otp, sasl]}}}
-									 ]});
-				_ ->
-					ok
-			end,
-			case raven_sup:start_link() of
-				{ok, Pid} ->
-					{ok, Pid};
-				Error ->
-					Error
-			end;
-		_ ->
-			{error, missing_configuration}
-	end.
+    case application:get_env(uri) of
+      {ok, _} ->
+        logger:add_handler(raven_otp_logger, raven_logger_backend, #{level => warning
+          , filter_default => log
+          , filters => [{ssl,      {fun logger_filters:domain/2, {stop, sub, [ssl]}}}
+                 ,{progress, {fun logger_filters:domain/2, {stop, equal, [progress]}}}
+                 ,{raven,    {fun logger_filters:domain/2, {stop, sub, [raven]}}}
+                       ,{sasl,     {fun logger_filters:domain/2, {stop, sub, [otp, sasl]}}}
+                 ]}),
+        case raven_sup:start_link() of
+          {ok, Pid} ->
+            {ok, Pid};
+          Error ->
+            Error
+        end;
+      _ ->
+        {error, missing_configuration}
+    end.
 
 %% @hidden
 stop(_State) ->
-	case application:get_env(error_logger) of
-		{ok, true} ->
-			error_logger:delete_report_handler(raven_error_logger),
-			ok;
-		_ ->
-			ok
-	end,
     inets:stop(httpc, ?RAVEN_HTTPC_PROFILE),
     ok.
 
